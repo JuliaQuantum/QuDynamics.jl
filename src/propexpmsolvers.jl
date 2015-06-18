a247e57c222a0ddc1da6ec262ffb7f4f447b4102
amitjamadagni <bitsjamadagni@gmail.com>
2015-06-18 21:44:42 +0530

    Packages added in travis. Options set in Expokit expmv type.

--- src/propexpmsolvers.jl
+++ src/propexpmsolvers.jl
@@ -9,15 +9,12 @@ QuExpm_Expo() = QuExpm_Expo(Dict())
 
 function propagate(prob::QuExpm_Expo, op, t, current_t, current_qustate)
     dt = t - current_t
-    next_state = Expokit.expmv(dt, -im*coeffs(op), coeffs(current_qustate))
+    next_state = Expokit.expmv(dt, -im*coeffs(op), coeffs(current_qustate), m = get(prob.options, :m, 30))
     return QuArray(next_state)
 end
 
 immutable QuExpm_ExpmV <: QuPropagatorMethod
-    options::Dict
 end
-
-QuExpm_ExpmV() = QuExpm_ExpmV(Dict())
 
 function propagate(prob::QuExpm_ExpmV, op, t, current_t, current_qustate)
     dt = t - current_t
