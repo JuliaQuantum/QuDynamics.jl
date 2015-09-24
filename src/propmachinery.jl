74ab7ae5aff30ba6bf69bc3bd50fd82ee1e4eb77
amitjamadagni <bitsjamadagni@gmail.com>
2015-09-24 17:22:02 +0800

    QuOperatorEvolution type, Tests, Docs.
    typealias of QuPropagator.

--- src/propmachinery.jl
+++ src/propmachinery.jl
@@ -1,7 +1,7 @@
 abstract QuPropagatorMethod
 
 @doc """
-QuPropagator Type
+QuPropagator and QuStateEvolution types are the same.
 
 The central piece which dispatches to various solvers. The various outer constructions, allow to
 give in various inputs. For example if the Hamiltonian is `QuBase.AbstractQuMatrix` and the  initial state
@@ -27,27 +27,30 @@ Inputs :
 Output :
 * QuPropagator construct depending on the input.
 """ ->
-immutable QuPropagator{QPM<:QuPropagatorMethod, QVM<:Union(QuBase.AbstractQuVector,QuBase.AbstractQuMatrix), QE<:QuEquation}
+immutable QuStateEvolution{QPM<:QuPropagatorMethod, QVM<:@compat(Union{QuBase.AbstractQuVector,QuBase.AbstractQuMatrix}), QE<:QuEquation}
     eq::QE
     init_state::QVM
     tlist
     method::QPM
-    QuPropagator(eq, init_state, tlist, method) = new(eq, init_state, tlist, method)
+    QuStateEvolution(eq, init_state, tlist, method) = new(eq, init_state, tlist, method)
 end
 
-QuPropagator{QPM<:QuPropagatorMethod, QV<:QuBase.AbstractQuVector}(eq::QuSchrodingerEq, init_state::QV, tlist, method::QPM) = QuPropagator{QPM,QV,QuSchrodingerEq}(eq, init_state, tlist, method)
+QuStateEvolution{QPM<:QuPropagatorMethod, QV<:QuBase.AbstractQuVector}(eq::QuSchrodingerEq, init_state::QV, tlist, method::QPM) = QuStateEvolution{QPM,QV,QuSchrodingerEq}(eq, init_state, tlist, method)
 
-QuPropagator{QPM<:QuPropagatorMethod, QV<:QuBase.AbstractQuVector}(hamiltonian::QuBase.AbstractQuMatrix, init_state::QV,  tlist, method::QPM) = QuPropagator{QPM,QV,QuSchrodingerEq}(QuSchrodingerEq(hamiltonian),init_state, tlist, method)
+QuStateEvolution{QPM<:QuPropagatorMethod, QV<:QuBase.AbstractQuVector}(hamiltonian::QuBase.AbstractQuMatrix, init_state::QV,  tlist, method::QPM) = QuStateEvolution{QPM,QV,QuSchrodingerEq}(QuSchrodingerEq(hamiltonian),init_state, tlist, method)
 
-QuPropagator{QPM<:QuPropagatorMethod, QM<:QuBase.AbstractQuMatrix}(eq::QuLiouvillevonNeumannEq, init_state::QM, tlist, method::QPM) = QuPropagator{QPM,QM,QuLiouvillevonNeumannEq}(eq, init_state, tlist, method)
+QuStateEvolution{QPM<:QuPropagatorMethod, QM<:QuBase.AbstractQuMatrix}(eq::QuLiouvillevonNeumannEq, init_state::QM, tlist, method::QPM) = QuStateEvolution{QPM,QM,QuLiouvillevonNeumannEq}(eq, init_state, tlist, method)
 
-QuPropagator{QPM<:QuPropagatorMethod, QM<:QuBase.AbstractQuMatrix}(hamiltonian::QuBase.AbstractQuMatrix, init_state::QM,  tlist, method::QPM) = QuPropagator{QPM,QM,QuLiouvillevonNeumannEq}(QuLiouvillevonNeumannEq(liouvillian_op(hamiltonian)),init_state, tlist, method)
+QuStateEvolution{QPM<:QuPropagatorMethod, QM<:QuBase.AbstractQuMatrix}(hamiltonian::QuBase.AbstractQuMatrix, init_state::QM,  tlist, method::QPM) = QuStateEvolution{QPM,QM,QuLiouvillevonNeumannEq}(QuLiouvillevonNeumannEq(liouvillian_op(hamiltonian)),init_state, tlist, method)
 
-QuPropagator{QPM<:QuPropagatorMethod, QM<:QuBase.AbstractQuMatrix}(eq::QuLindbladMasterEq, init_state::QM, tlist, method::QPM) = QuPropagator{QPM,QM,QuLindbladMasterEq}(eq, init_state, tlist, method)
+QuStateEvolution{QPM<:QuPropagatorMethod, QM<:QuBase.AbstractQuMatrix}(eq::QuLindbladMasterEq, init_state::QM, tlist, method::QPM) = QuStateEvolution{QPM,QM,QuLindbladMasterEq}(eq, init_state, tlist, method)
 
-QuPropagator{QPM<:QuPropagatorMethod, QM<:QuBase.AbstractQuMatrix, COT<:QuBase.AbstractQuMatrix}(hamiltonian::QuBase.AbstractQuMatrix, collapse_ops::Vector{COT}, init_state::QM, tlist, method::QPM) = QuPropagator{QPM,QM,QuLindbladMasterEq}(QuLindbladMasterEq(hamiltonian,collapse_ops), init_state, tlist, method)
+QuStateEvolution{QPM<:QuPropagatorMethod, QM<:QuBase.AbstractQuMatrix, COT<:QuBase.AbstractQuMatrix}(hamiltonian::QuBase.AbstractQuMatrix, collapse_ops::Vector{COT}, init_state::QM, tlist, method::QPM) = QuStateEvolution{QPM,QM,QuLindbladMasterEq}(QuLindbladMasterEq(hamiltonian,collapse_ops), init_state, tlist, method)
 
-QuPropagator{QPM<:QuPropagatorMethod, QV<:QuBase.AbstractQuVector, COT<:QuBase.AbstractQuMatrix}(hamiltonian::QuBase.AbstractQuMatrix, collapse_ops::Vector{COT}, init_state::QV, tlist, method::QPM) = QuPropagator{QPM,QV,QuLindbladMasterEq}(QuLindbladMasterEq(hamiltonian,collapse_ops), init_state, tlist, method)
+QuStateEvolution{QPM<:QuPropagatorMethod, QV<:QuBase.AbstractQuVector, COT<:QuBase.AbstractQuMatrix}(hamiltonian::QuBase.AbstractQuMatrix, collapse_ops::Vector{COT}, init_state::QV, tlist, method::QPM) = QuStateEvolution{QPM,QV,QuLindbladMasterEq}(QuLindbladMasterEq(hamiltonian,collapse_ops), init_state, tlist, method)
+
+# QuPropagator and QuStateEvolution types are the same.
+typealias QuPropagator QuStateEvolution
 
 @doc """
 QuPropagator State
@@ -71,7 +74,7 @@ Output :
 * QuPropagatorState construct.
 """ ->
 immutable QuPropagatorState
-    psi
+    state
     t
     t_state
 end
@@ -91,7 +94,7 @@ Inputs :
 Output :
 * QuPropagatorState construct with parameters as initial state, start time and time state.
 """ ->
-function Base.start(prob::QuPropagator)
+function Base.start(prob::QuStateEvolution)
     init_state = prob.init_state
     t_state = start(prob.tlist)
     t,t_state = next(prob.tlist,t_state)
@@ -109,7 +112,6 @@ Inputs :
 * prob :: QuPropagator{QuPropagatorMethod}
 
   The  `QuPropagator` contruct which involves the parameters of the system.
-
 * qustate :: QuPropagatorState
 
   Current QuPropagatorState
@@ -117,8 +119,8 @@ Inputs :
 Output :
 * Next state, Time corresponding to the current time and related QuPropagatorState
 """ ->
-function Base.next{QPM<:QuPropagatorMethod}(prob::QuPropagator{QPM}, qustate::QuPropagatorState)
-    current_qustate = qustate.psi
+function Base.next{QPM<:QuPropagatorMethod}(prob::QuStateEvolution{QPM}, qustate::QuPropagatorState)
+    current_qustate = qustate.state
     current_t = qustate.t
     t,t_state = next(prob.tlist, qustate.t_state)
     next_qustate = propagate(prob.method, prob.eq, t, current_t, current_qustate)
@@ -128,7 +130,7 @@ end
 @doc """
 Iterator for QuPropagator
 
-Iterator `next` method for QuPropagator
+Iterator `done` method for QuPropagator
 
 ### Arguments
 
@@ -140,7 +142,37 @@ Inputs :
 Output :
 * True if the current state is final state, else false.
 """ ->
-Base.done(prob::QuPropagator, qustate::QuPropagatorState) = done(prob.tlist, qustate.t_state)
+Base.done(prob::QuStateEvolution, qustate::QuPropagatorState) = done(prob.tlist, qustate.t_state)
 
-export  QuPropagator,
+@doc """
+Propagation of the time evolution operator.
+
+### Arguments
+
+Inputs :
+* op <: QuBase.AbstractQuMatrix
+
+  Operator to be evolved.
+* dt :: Float64
+
+  Time step.
+
+* tf, ti :: Float64
+
+  Final Time, Initial Time.
+
+Output :
+* Evolved operator.
+""" ->
+QuEvolutionOp{QM<:QuBase.AbstractQuMatrix}(op::QM, dt::Float64) = expm(-im*op*dt)
+
+QuEvolutionOp{QM<:QuBase.AbstractQuMatrix}(op::QM, tf::Float64,  ti::Float64) = QuEvolutionOp(op, tf-ti)
+
+QuEvolutionOp{QE<:QuEquation}(eq::QE, dt::Float64) = QuEvolutionOp(operator(eq), dt)
+
+QuEvolutionOp{QE<:QuEquation}(eq::QE, tf::Float64, ti::Float64) = QuEvolutionOp(eq, tf-ti)
+
+export  QuStateEvolution,
+      QuPropagator,
+      QuEvolutionOp,
       QuPropagatorState
